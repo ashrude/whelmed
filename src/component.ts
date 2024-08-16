2789f500ed0c559c6c9ba8a601741226f45f9d85
Ashley Rudelsheim <ashley@e.ashleyr.ca>
2024-08-16 13:34:08 -0700

    added updateView

--- src/component.ts
+++ src/component.ts
@@ -1,5 +1,15 @@
 import { Widget } from "./widgets/Widget";
 
+export interface Component<
+INPUT,
+OUTPUT,
+MODEL,
+INIT,
+WIDGETS
+> {
+    updateView?(): Promise<void> | void
+}
+
 export abstract class Component<
     INPUT,
     OUTPUT,
@@ -16,21 +26,25 @@ export abstract class Component<
 
     constructor(init: INIT) {
         this.model = this.init(init);
+
+        if(this.updateView)
+            this.updateView();
     };
 
     abstract init(init: INIT): MODEL;
 
-    input<K extends keyof INPUT>(key: K, value: INPUT[K]): void {
-        this.update(key, value);
+    async input<K extends keyof INPUT>(key: K, value: INPUT[K]): Promise<void> {
+        await this.update(key, value);
+        if(this.updateView)
+            await this.updateView();
     }
     output<K extends keyof OUTPUT>(key: K, value: OUTPUT[K]): void {
         if (this.#forwarder) {
             this.#forwarder(key, value);
         }
     }
-
     
-    abstract update<T extends keyof INPUT>(key: T, value: INPUT[T]): void;
+    abstract update<T extends keyof INPUT>(key: T, value: INPUT[T]): Promise<void> | void;
 
     forward(forwarder: <K extends keyof OUTPUT>(key: K, value: OUTPUT[K]) => void) {
         this.#forwarder = forwarder;
